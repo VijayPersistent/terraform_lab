fa4986e5271e67bc53890093d8ee6dc03e956404
VijayPersistent <95009494+VijayPersistent@users.noreply.github.com>
2022-07-24 12:06:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,38 @@
 provider "aws" {
-  profile = "myaws"
-  region  = var.region
+    region     = "us-east-1"
+    access_key = "AKIATDCIU2RZX2XH7YPM"
+    secret_key = "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"
 }
 
-
 resource "aws_s3_bucket" "b" {
-  bucket = "myawsbucket12345554555"
+  bucket = "myawsbucket1234554321"
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket143"
     Environment = "Dev"
   }
 }
 
-variable "region" {
+
+
+
+#provider "aws" {
+#  profile = "myaws"
+#  region  = var.region
+#}
+
+
+#resource "aws_s3_bucket" "b" {
+#  bucket = "myawsbucket12345554555"
+#  acl    = "private"
+
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+#}
+
+#variable "region" {
   
-}
+#}
